4eb55e1c8ea2a2d1ea7720c50c19d61545e37dc6
Congwu Tao <congwu.tao@gmail.com>
2012-12-03 10:33:30 -0500

    the second interface visualization

---
+++ d3/caterest.js
@@ -0,0 +1,5 @@
+[
+  {"Name":"Burge King1","Overall":"4","Vuseful":"5","Vcool":"4","Vfunny":"4","Ruseful":"5","Rcool":"4","Rfunny":"4","Rquntity":"4","Recent":"4"},
+  {"Name":"Burge King2","Overall":"5","Vuseful":"4","Vcool":"4","Vfunny":"3","Ruseful":"4","Rcool":"5","Rfunny":"3","Rquntity":"3","Recent":"3"},
+  {"Name":"Burge King3","Overall":"3","Vuseful":"4","Vcool":"3","Vfunny":"2","Ruseful":"3","Rcool":"2","Rfunny":"2","Rquntity":"2","Recent":"3"}
+]
