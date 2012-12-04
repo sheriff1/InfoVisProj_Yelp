700ccfe57f370776740b94a877206287474e2b6a
Congwu Tao <congwu.tao@gmail.com>
2012-12-04 12:55:20 -0500

    samlpe bar chart-- works well

--- InfoVis_Yelp/src/yelp/SimpleBar.java
+++ InfoVis_Yelp/src/yelp/SimpleBar.java
@@ -1,72 +1,83 @@
 package yelp;
 
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.util.LinkedHashMap;
 import java.util.Map;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 
 public class SimpleBar extends JPanel
+
 {
-	private Map<Color, Integer> bars =
-            new LinkedHashMap<Color, Integer>();
+	int width = 60;  // the initial width of bar
+	int height = 20; // the initial height of bar
+	
+	private Map<Color, Integer> bars = new LinkedHashMap<Color, Integer>();
 	
 	/**
 	 * Add new bar to chart
 	 * @param color color to display bar
 	 * @param value size of bar
 	 */
-	public void addBar(Color color, int value)
+	public void addBar(Color color,int value)
 	{
-		bars.put(color, value);
+		bars.put(color,value);
 		repaint();
 	}
 	
 	@Override
 	protected void paintComponent(Graphics g)
 	{
-		// determine longest bar
-		
-		int max = Integer.MIN_VALUE;
-		for (Integer value : bars.values())
-		{
-			max = Math.max(max, value);
-		}
 		
 		// paint bars
+		int x = 10; //initial X-axis
+		int y = 120; //inital Y-axis
+		int i = 0;
 		
-		int width = (getWidth() / bars.size()) - 2;
-		int x = 1;
 		for (Color color : bars.keySet())
 		{
-			int value = bars.get(color);
-			int height = (int) 
-                            ((getHeight()-5) * ((double)value / max));
+			
 			g.setColor(color);
-			g.fillRect(x, getHeight() - height, width, height);
-			g.setColor(Color.black);
-			g.drawRect(x, getHeight() - height, width, height);
-			x += (width + 2);
+			g.fillRect(x, y, width, height);
+			g.setColor(Color.BLACK);
+			g.drawRect(x, y, width, height);
+			
+			i++;
+			if(i%9 == 0) {
+				x= 10;
+				y=(y+50);
+			}else{
+				x += (width + 10);
+			}
+			
 		}
 	}
+	
+	
 
-	@Override
-	public Dimension getPreferredSize() {
-		return new Dimension(bars.size() * 10 + 2, 50);
-	}
 
 	public static void main(String[] args)
 	{
+		
+		
 		JFrame frame = new JFrame("Bar Chart");
+		
 		SimpleBar chart = new SimpleBar();
-		chart.addBar(Color.red, 100);
-		chart.addBar(Color.green, 8);
-		chart.addBar(Color.blue, 54);
-		chart.addBar(Color.black, 23);		
+		chart.addBar(Color.red, 20);
+		chart.addBar(Color.green, 20);
+		chart.addBar(Color.blue, 20);
+		chart.addBar(Color.yellow, 20);	
+		chart.addBar(Color.magenta, 20);
+		chart.addBar(Color.cyan, 20);
+		chart.addBar(Color.gray, 20);
+		chart.addBar(Color.pink, 20);
+		chart.addBar(Color.ORANGE, 20);	
+		chart.addBar(Color.lightGray, 20);
+		chart.addBar(Color.black, 20);
+		chart.addBar(Color.darkGray, 20);
+		
 		frame.getContentPane().add(chart);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
